50727a7a26f7139812213eaa78a054e59fc82c5e
Bao Ngoc <ngoc.nguyenkhmt@hcmut.edu.vn>
2021-11-07 07:43:46 +0700

    2904

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import MyAppBar from './MyAppBar';
+// import MyContent from './MyContent';
+import MyClass from './MyClass';
+import moment from 'moment';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = { selectedClass: '', addStudent: false };
+	}
+
+	handleClassChange = (selectedClass) => {
+		console.log('App chọn: ', selectedClass);
+		this.setState({
+			selectedClass: selectedClass,
+			newStudent: null,
+			className: this.state.selectedClass,
+		});
+	};
+
+	handleAddStudent = () => {
+		console.log('App thêm sinh viên vào lớp: ', this.state.selectedClass);
+		this.addNewStudent();
+	};
+
+	addNewStudent = () => {
+		// console.log('addNewStudent');
+		fetch('https://randomuser.me/api/?results=1')
+			.then((res) => res.json())
+			.then(
+				(data) => {
+					// console.log('data', data.results);
+					let id = 1;
+					const dataWithId = data.results.map((record) => {
+						return {
+							id: id++,
+							firstName: record.name.first,
+							lastName: record.name.last,
+							country: record.location.country,
+							phone: record.phone,
+							dob: moment(record.dob.date).format('DD/MM/YYYY'),
+							picture: record.picture.thumbnail,
+						};
+					});
+					// console.log('dataWithId', dataWithId);
+					this.setState({
+						newStudent: dataWithId[0],
+						className: this.state.selectedClass,
+					});
+				},
+				(error) => {
+					console.log('error', error);
+				}
+			);
+	};
+
+	render() {
+		return (
+			<div>
+				<MyAppBar
+					classes={[{ id: 1, value: 'một' }]}
+					handleSelectClassChange={this.handleClassChange}
+					handleAddStudent={this.handleAddStudent}
+				/>
+				<br />
+				<MyClass
+					newStudent={this.state.newStudent}
+					className={this.state.selectedClass}
+				/>
+			</div>
+		);
+	}
 }
 
 export default App;
